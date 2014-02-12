fbf37ac9604dda976c624147a0be18148022863a
Max <silverhammermba@gmail.com>
2014-02-11 22:54:53 -0500

    Load shaders

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
 using std::cerr;
 using std::endl;
+
+std::string read_file(const std::string& filename)
+{
+	std::string content;
+
+	std::ifstream in(filename);
+	if (!in)
+		throw std::runtime_error("Failed to read file: " + filename);
+
+	in.seekg(0, std::ios::end);
+	content.resize(in.tellg());
+	in.seekg(0, std::ios::beg);
+	in.read(&content[0], content.size());
+	in.close();
+
+	return content;
+}
+
+void load_shader(GLuint shader, const std::string& filename)
+{
+	std::string source = read_file(filename);
+	const char* ptr = source.c_str();
+	glShaderSource(shader, 1, (const GLchar**)&ptr, nullptr);
+	glCompileShader(shader);
+	// error check
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	GLint log_length;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+	if (log_length > 0)
+	{
+		char buffer[log_length];
+		glGetShaderInfoLog(shader, log_length, nullptr, buffer);
+		cerr << buffer;
+	}
+	if (status == GL_FALSE)
+		throw std::runtime_error("Failed to compile shader: " + filename);
+}
 
 int main(int argc, char** argv)
 {
@@ -49,6 +91,52 @@ int main(int argc, char** argv)
 		cerr << "glewInit failed: " << glewGetErrorString(error) << endl;
 		return 1;
 	}
+
+	float vertices[] = {
+		 0.0f,  0.5f,
+		 0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+
+	// create buffer
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+
+	// set active
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	// load data
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// compile shaders
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	load_shader(vertex_shader, "vertex.glsl");
+
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	load_shader(fragment_shader, "fragment.glsl");
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+
+	glLinkProgram(program);
+	GLint status;
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	GLint log_length;
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length);
+	if (log_length > 0)
+	{
+		char buffer[log_length];
+		glGetProgramInfoLog(program, log_length, nullptr, buffer);
+		cerr << buffer;
+	}
+	if (status == GL_FALSE)
+	{
+		cerr << "Failed to link program\n";
+		return 1;
+	}
+
+	glUseProgram(program);
 
 	// main loop
 	SDL_Event event;
